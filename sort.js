6076d2fbea2b74fdabba0efaafb3642c61a92f59
Leila Hadjioual <leila.hadjioual@Digital-Grenoble.local>
2018-06-21 15:40:34 +0200

    modif

--- sort.js
+++ sort.js
@@ -20,21 +20,19 @@ function insertSort() {
 let b = [3, 9, 7, 1, 6, 2, 8, 4, 5];
 
 function selectionSort() {
-    // console.log("*******tableau tri sélection avant tri ******* " + b);
+    console.log("*******tableau tri sélection avant tri ******* " + b);
     let tmp;
     for (let i = 0; i < b.length; i++) {
         let k = i;
         for (let j = i + 1; j < b.length; j++) {
             if (b[j] <= b[k]) {
                 k = j;
-                if (b[k] < b[i]) {
-                    tmp = b[k];
-                    b[k] = b[i];
-                    b[i] = tmp;
-                    // console.log("tableau après la boucle sélection " + b);
-                }
             }
         }
+        tmp = b[k];
+        b[k] = b[i];
+        b[i] = tmp;
+        console.log("tableau après la boucle sélection " + b);
     }
 }
 
@@ -53,7 +51,7 @@ function quickSort(iDeb, iFin) {
     }
     swap(iDeb, k);
 
-    //ce code est fait après la boucle terminée, divise le tableau en 2 (k étant le pivot)
+    //ce code est fait après la boucle terminée, divise le tableau en 2 en fonction de la position k (k étant le pivot)
     if (iDeb < k - 1) {
         quickSort(iDeb, k - 1);
     }
